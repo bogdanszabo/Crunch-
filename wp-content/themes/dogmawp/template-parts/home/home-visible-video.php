754fe0d51ffc4aae0565856e8ba434b0e7ee5d09
Bogdan <bogdanszabo@yahoo.com>
2016-02-14 23:00:05 +0100

    just trying

--- wp-content/themes/dogmawp/template-parts/home/home-visible-video.php
+++ wp-content/themes/dogmawp/template-parts/home/home-visible-video.php
@@ -12,20 +12,25 @@
         <div class="full-height-wrap">
             <div class="full-width-slider-holder">
                 <?php if(get_post_meta($post->ID,'rnr_portfolio-post-formats',true)=='portfolio'){ ?>
-                <!--media-container -->
-                <div class="media-container">
-                    <div class="video-mask"></div>
-                    <!--=============== add you video id  in data-vid="" if you want add sound change data-mv="1" on data-mv="0" ===============-->
-                    <?php if (( get_post_meta($post->ID,'rnr_yid',true))):?>
-                        <div  class="background-video" data-vid="<?php echo esc_attr(get_post_meta($post->ID,'rnr_yid',true)); ?>" data-mv="<?php echo (get_post_meta($post->ID,'rnr_intro-section-video-sound',true)) ?>"> </div>
-                    <?php endif;?>
-                    <?php if (has_post_thumbnail( $post->ID ) ):
-                        $wr_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '' );?>
-                        <div class="bg mob-bg" style="background-image:url(<?php echo esc_url($wr_image[0]);?>)"></div>
-                    <?php endif;?>
-                </div>
-                <!--media-container end -->
-                <div class="overlay"></div>
+                    <!--media-container -->
+                    <div class="media-container">
+                        <div class="video-mask"></div>
+                        <!--=============== add you video id  in data-vid="" if you want add sound change data-mv="1" on data-mv="0" ===============-->
+                        <?php if (( get_post_meta($post->ID,'rnr_yid',true))):?>
+                            <div  class="background-video" data-vid="<?php echo esc_attr(get_post_meta($post->ID,'rnr_yid',true)); ?>" data-mv="<?php echo (get_post_meta($post->ID,'rnr_intro-section-video-sound',true)) ?>"> </div>
+                        <?php endif;?>
+                        <?php if (has_post_thumbnail( $post->ID ) ):
+                            $wr_image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), '' );?>
+                            <div class="bg mob-bg" style="background-image:url(<?php echo esc_url($wr_image[0]);?>)"></div>
+                        <?php endif;?>
+                    </div>
+                    <!--media-container end -->
+                    <div class="overlay"></div>
+                <?php }?>
+
+
+
+            </div>
         </div>
         <!-- full-height-wrap end  -->
     </div>
