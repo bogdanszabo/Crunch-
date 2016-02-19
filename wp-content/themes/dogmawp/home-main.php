de6042c7fbc31e83092d19e4d2329336abb249b0
Bogdan <bogdanszabo@yahoo.com>
2016-02-19 19:47:28 +0100

    reversed

--- wp-content/themes/dogmawp/home-main.php
+++ wp-content/themes/dogmawp/home-main.php
@@ -2,36 +2,36 @@
 <?php $wr_options = get_option('wr_wp');?>
 <?php
 /*Template Name:Home Page Template*/
- get_header();
- 
- 
- ?>
- 
- <?php if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st2'){ ?> 
-         <?php get_template_part('template-parts/home/home-slide');?>
-		 <?php }
-		 else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st3'){ ?> 
-         <?php get_template_part('template-parts/home/home-multi-4');?>
-		 <?php }
-		 else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st4'){ ?> 
-         <?php get_template_part('template-parts/home/home-multi-3');?>
-		 <?php }
-		 else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st5'){ ?> 
-         <?php get_template_part('template-parts/home/home-video');?>
-		 <?php }
-		 else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st6'){ ?> 
-         <?php get_template_part('template-parts/home/home-single-image');?>
-		 <?php }
-		 else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st7'){ ?> 
-         <?php get_template_part('template-parts/home/home-slider');?>
-		 <?php }
-		  else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st8'){ ?> 
-         <?php get_template_part('template-parts/home/home-visiable-menu');?>
-		 <?php }
-		  else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st9'){ ?>
-		 <?php get_template_part('template-parts/home/home-visible-video');?>
-		  <?php }
-		 else  { ?>
-		 <?php get_template_part('template-parts/home/home-slide');?>
-		 <?php }?>
+get_header();
+
+
+?>
+
+<?php if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st2'){ ?>
+	<?php get_template_part('template-parts/home/home-slide');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st3'){ ?>
+	<?php get_template_part('template-parts/home/home-multi-4');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st4'){ ?>
+	<?php get_template_part('template-parts/home/home-multi-3');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st5'){ ?>
+	<?php get_template_part('template-parts/home/home-video');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st6'){ ?>
+	<?php get_template_part('template-parts/home/home-single-image');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st7'){ ?>
+	<?php get_template_part('template-parts/home/home-slider');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st8'){ ?>
+	<?php get_template_part('template-parts/home/home-visiable-menu');?>
+<?php }
+else if(get_post_meta($post->ID,'rnr_wr_home_pagetype',true)=='st9'){ ?>
+	<?php get_template_part('template-parts/home/home-visible-video');?>
+<?php }
+else  { ?>
+	<?php get_template_part('template-parts/home/home-slide');?>
+<?php }?>
 <?php get_footer(); ?>	
